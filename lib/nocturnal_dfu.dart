d98af392d49ba7a5380294c4b35207a4df4c10ad
Bhargav Lakkur Kusha Kumar <bhargav@Bhargavs-MacBook-Pro.local>
2025-06-07 13:41:22 -0400

    Fixed file names

--- lib/nocturnal_dfu.dart
+++ lib/nocturnal_dfu.dart
@@ -5,5 +5,6 @@ library;
 
 export 'src/nocturnal_dfu_base.dart';
 export 'src/models/ota_related.dart';
+export 'src/manifest_parser.dart';
 
 // TODO: Export any libraries intended for clients of this package.
