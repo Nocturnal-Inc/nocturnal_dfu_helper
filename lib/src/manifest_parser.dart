95e4b08412d3c705fd81e120a50f7bc5bfa1b8ac
Bhargav Lakkur Kusha Kumar <bhargav@Bhargavs-MacBook-Pro.local>
2025-07-28 13:13:16 -0400

    Updated with logs and wrapped the code with try catch

--- lib/src/manifest_parser.dart
+++ lib/src/manifest_parser.dart
@@ -6,40 +6,56 @@ import 'package:nocturnal_dfu/src/models/ota_manifest.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 
-Future<(OTAManifest?, String)> extractAndReadManifest(
+Future<(OTAManifest?, String, String?)> extractAndReadManifest(
   String zipFilePath,
 ) async {
-  final Directory tempDir = await getTemporaryDirectory();
-  final String outputDirPath = p.join(tempDir.path, 'unzipped_bundle');
+  try {
+    print("1");
+    final Directory tempDir = await getTemporaryDirectory();
+    final String outputDirPath = p.join(tempDir.path, 'unzipped_bundle');
 
-  // Create/clear output directory
-  final outDir = Directory(outputDirPath);
-  if (await outDir.exists()) {
-    await outDir.delete(recursive: true);
-  }
-  await outDir.create(recursive: true);
+    print("2");
 
-  final zipBytes = await File(zipFilePath).readAsBytes();
-  final archive = ZipDecoder().decodeBytes(zipBytes);
-
-  for (final file in archive) {
-    final String outFilePath = p.join(outputDirPath, file.name);
-    if (file.isFile) {
-      final outFile = File(outFilePath)..createSync(recursive: true);
-      await outFile.writeAsBytes(file.content as List<int>);
-    } else {
-      await Directory(outFilePath).create(recursive: true);
+    // Create/clear output directory
+    final outDir = Directory(outputDirPath);
+    if (await outDir.exists()) {
+      await outDir.delete(recursive: true);
     }
-  }
+    await outDir.create(recursive: true);
 
-  final manifestFile = File(p.join(outputDirPath, 'manifest.json'));
-  if (await manifestFile.exists()) {
-    final jsonStr = await manifestFile.readAsString().timeout(
-      Duration(seconds: 3),
-    );
-    final manifest = OTAManifest.fromJson(jsonDecode(jsonStr));
-    return (manifest, outputDirPath);
-  }
+    print("3");
 
-  return (null, outputDirPath);
+    final zipBytes = await File(zipFilePath).readAsBytes();
+    final archive = ZipDecoder().decodeBytes(zipBytes);
+
+    print("4");
+
+    for (final file in archive) {
+      final String outFilePath = p.join(outputDirPath, file.name);
+      print("4.1 $outFilePath");
+      if (file.isFile) {
+        final outFile = File(outFilePath)..createSync(recursive: true);
+        await outFile.writeAsBytes(file.content as List<int>);
+      } else {
+        await Directory(outFilePath).create(recursive: true);
+      }
+    }
+
+    print("5");
+
+    final manifestFile = File(p.join(outputDirPath, 'manifest.json'));
+    if (await manifestFile.exists()) {
+      final jsonStr = await manifestFile.readAsString().timeout(
+        Duration(seconds: 3),
+      );
+      final manifest = OTAManifest.fromJson(jsonDecode(jsonStr));
+      return (manifest, outputDirPath, null);
+    }
+
+    print("6 $outputDirPath");
+
+    return (null, outputDirPath, null);
+  } catch (e, s) {
+    return (null, "", "Error: $e | Stacktrace: $s");
+  }
 }
